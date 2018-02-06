0f2d4d818bfacd5bc53ae669b12c3a29aa1a3335
nickode <nicolasnsamaha@hotmail.com>
2018-02-06 17:16:32 -0500

    Added dog list

--- load.js
+++ load.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-	console.log("BISH IM READY!!");
-
-	$("h1").animate(
-	{
-		color:"red"
-		
-	}, 
-
-	1000, 
-
-	function(){
-		console.log("done!");
-	}
-
-	);
+    
+});
 	
-})
